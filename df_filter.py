9657cb94939cb1b9de2390c53a0a14d1c75f5965
markstavr <marknata64@gmail.com>
2022-04-21 16:01:29 +0300

    Update df_filter.py

--- df_filter.py
+++ df_filter.py
@@ -15,13 +15,13 @@ def df_flt(sel_val, df):
 st.sidebar.title("Filter selectbox")
 lst = ['Все']; lst= lst + list(df['A'].drop_duplicates().sort_values().values)
 sel_val=st.sidebar.selectbox('выберите значения', lst, 0)
-st.sidebar.write('Выбрано:', sel_val, type(sel_val))
+st.sidebar.write('Выбрано:', sel_val)
 st.caption("DataFrame Filter selectbox")
 st.dataframe(df_flt(sel_val, df))
 # отбор с использованием multiselect
 st.sidebar.title("Filter multiselect")
 sel_val_ms =st.sidebar.multiselect('выберите значения', lst,  'Все')
-st.sidebar.write('Выбрано:', sel_val_ms, type(sel_val))
+st.sidebar.write('Выбрано:', sel_val_ms)
 def df_flt_ms(sel_val_lst, df):
   if 'Все' in sel_val_lst or len(sel_val_lst)==0:
     return df
