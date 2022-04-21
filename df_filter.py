0a8eaeca561032ace264c7150f863719e4b64d03
markstavr <marknata64@gmail.com>
2022-04-21 15:17:44 +0300

    Update df_filter.py

--- df_filter.py
+++ df_filter.py
@@ -4,7 +4,7 @@ import numpy as np
 np.random.seed(0)
 df = pd.DataFrame(np.round(np.random.randn(5, 1),3), columns=list('A'))
 # Настройка заголовка и текста 
-st.title("DATAFRAME FILTER")
+st.title("GПример фильтрации Pandas DataFrame")
 #st.dataframe(df)
 
 def df_flt(sel_val, df):
@@ -13,11 +13,14 @@ def df_flt(sel_val, df):
   else:
     s1=df['A']==sel_val
     return df[s1]
-
+# отбор с использованием selectbox
 st.sidebar.title("Filter")
 lst = ['Все']; lst= lst + list(df['A'].drop_duplicates().sort_values().values)
 #lst= df['A'].drop_duplicates().sort_values().values
-#sel_val=st.sidebar.multiselect('выберите значения', lst,  'Все')
 sel_val=st.sidebar.selectbox('выберите значения', lst, 0)
 st.sidebar.write('Выбрано:', sel_val, type(sel_val))
 st.dataframe(df_flt(sel_val, df))
+# отбор с использованием multiselect
+sel_val_ms =st.sidebar.multiselect('выберите значения', lst,  'Все')
+st.sidebar.write('Выбрано:', sel_val_ms, type(sel_val))
+st.dataframe(df_flt(sel_val_ms, df))
