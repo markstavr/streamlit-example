58682b3d5010e468cf1fd8bff086e289fe44081e
markstavr <marknata64@gmail.com>
2022-04-21 12:00:58 +0300

    Update df_filter.py

--- df_filter.py
+++ df_filter.py
@@ -17,6 +17,7 @@ def df_flt(sel_val, df):
 st.sidebar.title("Filter")
 lst = ['Все']; lst= lst + list(df['A'].drop_duplicates().sort_values().values)
 #lst= df['A'].drop_duplicates().sort_values().values
-sel_val=st.sidebar.multiselect('выберите значения', lst,  'Все')
+#sel_val=st.sidebar.multiselect('выберите значения', lst,  'Все')
+sel_val=st.sidebar.selectbox('выберите значения', lst,  'Все')
 st.sidebar.write('Выбрано:', sel_val, type(sel_val))
 #st.dataframe(df_flt(sel_val, df))
